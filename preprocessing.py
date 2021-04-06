1e7df419ee7a614aea7a4334abb4542c7bf75d51
Shaumik-Ashraf <a.shaumik@gmail.com>
2021-04-06 12:06:16 -0400

    factorize preprocessing code, try rouge but module not found

--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,7 @@ train.csv contains two columns with each column wrapped in double quotes; the fi
 the input text (radiology examination, technique, comparison, and findings) while the second
 column contains the output text (impressions). All reports without the term "IMPRESSIONS:" are ommitted
 
-test.csv has the same structure as train.csv. 
+test.csv has the same structure as train.csv.
 
 The processing also lematizes all of the terms using nltk and strips whitespace.
 
@@ -28,78 +28,124 @@ LIST_FILE = os.path.join(ROOT, 'data', 'cxr-study-list.csv.gz');
 REPORTS_DIR = os.path.join(ROOT, 'data', 'mimic-cxr-reports');
 TRAIN_FILE = os.path.join(ROOT, 'data', 'train.csv');
 TEST_FILE = os.path.join(ROOT, 'data', 'test.csv');
+VALIDATION_FILE = os.path.join(ROOT, 'data', 'validation.csv');
+
+
+
+def remove_notification_section(text):
+	"""
+	We noticed that some reports have a notification section after
+	the impressions (summary) section, which was impeding our data, so
+	we decided to remove this section all together. We use various rule-based
+	mechanisms to parse and remove the notification section.
+
+	params: text
+	returns: text with notification section removed
+	"""
+	idx = text.rfind("NOTIFICATION");
+	if( idx > 0 ):
+		text = text[:idx];
+	idx = text.rfind("telephone notification");
+	if( idx > 0 ):
+		text = text[:idx];
+	idx = text.rfind("Telephone notification");
+	if( idx > 0 ):
+		text = text[:idx];
+	return(text);
+
+def sanitize(text):
+	"""
+	Cleanses the text to be written in CSV, which will be fed directly to
+	the summarizer. Tokenization and lemmatization is not performed in this
+	step, as the summarizer performs those directly.
+
+	params: text
+	returns: cleaned text
+	"""
+	text = text.strip();
+	text = re.sub("\n", "", text);
+	text = re.sub(",", "", text);
+	text = remove_notification_section(text);
+	return(text);
+
+def split(slicable, fraction):
+	"""
+	splits data into test-train set or dev-validation set; does not shuffle.
+
+	params: slicable - an object that responds to len() and [], works on dataframes
+	        fraction - a value between 0 and 1
+	returns: (x, y) - where x has (1-fraction) percent entries and y has the rest
+	"""
+	partition = int(len(slicable) * (1.0 - fraction));
+	return( (slicable[:partition], slicable[partition:]) );
+
+def parse_summary(text):
+	"""
+	parses and separates input text from summary in cxr reports, returns None if
+	not found
+
+	params: text
+	returns: None or [input_text, summary]
+	"""
+
+	regex = r'impression.?(?::|" ")'
+
+	if( re.search(regex, text, flags=re.IGNORECASE)==None ): #if no summary
+		return None;
+
+	data = re.split(r'impression.?(?::|" ")', text, flags=re.IGNORECASE);
+	data[0] = data[0].strip();
+	data[1] = data[1].strip();
+
+	return(data);
+
+def write_csv(filename, reports):
+	"""
+	writes a csv file for summarization. The CSV file has four columns: "subject_id",
+	"study_id", "findings", and "impression" based on MIMIC-CXR reports. "findings"
+	contains the input text, and "impression" contains the true summary.
+
+	params: filename - name of csv file to write, will overwrite if it exists
+		reports - dataframe of cxr reports from cxr-study-list file
+	"""
+	print(f"Writing {filename}...");
+	f = open(filename, 'w');
+	f.write(f"\"subject_id\",\"study_id\",\"findings\",\"impression\"\n");
+	ommitted = 0;
+	progress = 1;
+	for report in train_reports:
+		x = open(os.path.join(REPORTS_DIR, report));
+		text = x.read();
+		x.close();
+		text = sanitize(text);
+
+		if (progress % 10000 == 0):
+			print(f'Read {progress} files so far...');
+		progress += 1;
+
+		data = parse_summary(text);
+		if( data==None ):
+			ommitted += 1;
+			continue; #toss out data and go to next textfile
+
+		folders = report.split('/');
+		f.write(f"\"{folders[2]}\",\"{folders[3].split('.')[0]}\",\"{data[0]}\",\"{data[1]}\"\n");
+	f.close();
+	print(f"Ommited {ommitted} files out of {progress} total files in train.\n")
+	print("Done.\n");
 
 
 print("================ Starting data preprocessing ==================");
+
 print(f"Reading {os.path.basename(LIST_FILE)}...");
 radiology_reports = pd.read_csv(LIST_FILE)['path']; # file paths as pandas series
-train_len = int(len(radiology_reports) * (1.0 - TEST_FRACTION));
-train_reports = radiology_reports[:train_len];
-test_reports = radiology_reports[train_len:];
+train_reports, test_reports = split(radiology_reports, TEST_FRACTION);
+# if you want validation set:
+# train_reports, validation_reports = split(train_reports, 0.2);
 print("Done.");
 
-print("Writing train.csv...");
-f = open(TRAIN_FILE, 'w');
-f.write(f"\"subject_id\",\"study_id\",\"findings\",\"impression\"\n");
-ommitted = 0;
-progress = 1;
-for report in train_reports:
-	x = open(os.path.join(REPORTS_DIR, report));
-	text = x.read().strip();
-	text = re.sub("\n", "", text);
-	text = re.sub(",", "", text); # causes issues with CSV
-	idx = text.find("NOTIFICATION");
-	if( idx > 0 ):
-		text = text[:idx];
-	idx = text.find("telephone notification");
-	if( idx > 0 ):
-		text = text[:idx];
-	idx = text.find("Telephone notification");
-	if( idx > 0 ):
-		text = text[:idx];
-	data = re.split(r'impression.?(?::|" ")',text, flags=re.IGNORECASE);
-	data = [s.strip() for s in data]
-	if (progress % 10000 == 0):
-		print(f'Read {progress} files so far...');
-	progress += 1;
-	if( (len(data)<2) or (data[1].strip() == "") ):
-		ommitted += 1;
-		continue; #toss out data and go to next textfile
-	folders = report.split('/');
-	f.write(f"\"{folders[2]}\",\"{folders[3].split('.')[0]}\",\"{data[0]}\",\"{data[1]}\"\n");
-f.close();
-print(f"Ommited {ommitted} files out of {progress} total files in train.\n")
-print("Done.\n");
-print("Writing test.csv...");
-f = open(TEST_FILE, 'w');
-f.write(f"\"subject_id\",\"study_id\",\"findings\",\"impression\"\n");
-ommitted = 0;
-progress = 1;
-for report in test_reports:
-	x = open(os.path.join(REPORTS_DIR, report));
-	text = x.read().strip();
-	text = re.sub("\n", "", text);
-	text = re.sub(",", "", text); # causes issues with CSV
-	idx = text.find("NOTIFICATION");
-	if( idx > 0 ):
-		text = text[:idx];
-	idx = text.find("telephone notification");
-	if( idx > 0 ):
-		text = text[:idx];
-	idx = text.find("Telephone notification");
-	if( idx > 0 ):
-		text = text[:idx];
-	data = re.split(r'impression.?(?::|" ")',text, flags=re.IGNORECASE);
-	data = [s.strip() for s in data]
-	if (progress % 10000 == 0):
-		print(f'Read {progress} files so far...');
-	progress += 1;
-	if( (len(data)<2) or (data[1].strip() == "") ):
-		ommitted += 1;
-		continue; #toss out data and go to next textfile
-	folders = report.split('/');
-	f.write(f"\"{folders[2]}\",\"{folders[3].split('.')[0]}\",\"{data[0]}\",\"{data[1]}\"\n");
-f.close();
-print(f"Ommited {ommitted} files out of {progress} total files in test.\n")
-print("Done.\n");
+write_csv(TRAIN_FILE, train_reports);
+write_csv(TEST_FILE, test_reports);
+#write_csv(VALIDATION_FILE, validation_reports);
+
 print("==================== End data preprocessing ======================");
---
+++ rouge_score.py
@@ -0,0 +1,11 @@
+import rogue;
+
+scorer = rouge.Rouge(metrics=['rouge-l'], max_n=4, limit_length=True, length_limit=100,
+                     length_limit_type='words', apply_avg=True, apply_best=False,
+                     alpha=0.5, weight_factor=1.0, stemming=True)
+
+generated = "China 's government said Thursday that two prominent dissidents arrested this week are suspected of endangering national security _ the clearest sign yet Chinese leaders plan to quash a would-be opposition party .\nOne leader of a suppressed new political party will be tried on Dec. 17 on a charge of colluding with foreign enemies of China '' to incite the subversion of state power , '' according to court documents given to his wife on Monday .\nWith attorneys locked up , harassed or plain scared , two prominent dissidents will defend themselves against charges of subversion Thursday in China 's highest-profile dissident trials in two years .\n"
+truth = "Hurricane Mitch, category 5 hurricane, brought widespread death and destruction to Central American.\nEspecially hard hit was Honduras where an estimated 6,076 people lost their lives.\nThe hurricane, which lingered off the coast of Honduras for 3 days before moving off, flooded large areas, destroying crops and property.\nThe U.S. and European Union were joined by Pope John Paul II in a call for money and workers to help the stricken area.\nPresident Clinton sent Tipper Gore, wife of Vice President Gore to the area to deliver much needed supplies to the area, demonstrating U.S. commitment to the recovery of the region.\n"
+
+prec, recall, f1_score = scorer.get_scores([generatored], [truth]);
+print(prec, recall, f1_score);
