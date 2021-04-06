be8469b5d504120556a2c087c804d53ff7a00683
Shaumik-Ashraf <a.shaumik@gmail.com>
2021-04-06 16:30:49 -0400

    debugging bart_summarizer

--- bart_summarizer.py
+++ bart_summarizer.py
@@ -22,8 +22,8 @@ ROOT = os.path.dirname( os.path.abspath(__file__) );
 #VALIDATION_FILE = os.path.join(ROOT, 'data', 'validation.csv');
 TEST_FILE = os.path.join(ROOT, 'data', 'test.csv');
 
-LIMIT = 10;
-SUMMARIES_FILE = os.path.join(ROOT, 'data', f"summaries_{limit}.csv");
+LIMIT = 10; #set limit to -1 to do all data
+SUMMARIES_FILE = os.path.join(ROOT, 'data', f"summaries_{LIMIT}.csv");
 
 def load_file(filename):
 	"""
@@ -32,7 +32,9 @@ def load_file(filename):
 	param: filename - path to csv file
 	return: 2D numpy of csv data with text
 	"""
+	print(f"Loading data from {filename}...");
 	df = pd.read_csv(filename)
+	print(f"Done.");
 	return( np.array(df) );
 
 def load_bart(model_name='facebook/bart-large-cnn', tokenizer_name='facebook/bart-large'):
@@ -43,8 +45,12 @@ def load_bart(model_name='facebook/bart-large-cnn', tokenizer_name='facebook/bar
 		    tokenizer_name - pretrained BART huggingface tokenizer download path, default: facebook/bart-large
 	return: (model, tokenizer)
 	"""
+	print(f"Loading pretrained model {model_name}...");
 	model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+	print("Done.");
+	print(f"Loading pretrained tokenizer {tokenizer_name}...");
 	tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
+	print("Donw.");
 	return((model, tokenizer));
 
 def baseBart(article_to_summarize, model, tokenizer):
@@ -67,20 +73,24 @@ print("==================== Start abstractive summarization ====================
 data = load_file(TEST_FILE);
 model, tokenizer = load_bart();
 
-
 print(f"Writing {os.path.basename(SUMMARIES_FILE)}...");
 f = open(SUMMARIES_FILE, 'w');
 f.write(f"\"subject_id\",\"study_id\",\"prediction\",\"actual\"\n");
-
+i = 0;
 if LIMIT==-1:
 	for row in data:
 		comp_summary = baseBart(row[2], model, tokenizer)
 		f.write(f"\"{row[0]}\",\"{row[1]}\",\"{comp_summary}\",\"{row[3]}\"\n");
+		if( i%1000 == 0 ):
+			print(f"Computed {i} summaries");
+		i += 1;
 else:
 	for row in data[:LIMIT]:
 		comp_summary = baseBart(row[2], model, tokenizer)
 		f.write(f"\"{row[0]}\",\"{row[1]}\",\"{comp_summary}\",\"{row[3]}\"\n");
-
+		if( i%(int(LIMIT/8)) == 0 ):
+			print(f"Computed {i} summaries");
+		i += 1;
 
 f.close();
 print("Done.\n");
